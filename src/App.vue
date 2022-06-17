650df1071eea0a17709e1da0f3d51dbf6b98e6c9
PRH <JavaMissile@aliyun.com>
2022-06-17 12:29:05 +0800

    one

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,24 @@
 <template>
+
   <div >
+<Header></Header>
     sldkl
+    <Footer></Footer>
   </div>
 </template>
 
 <script>
-
+import Header from "./components/Header/header.vue"
+import Footer from "./components/Footer/footer.vue"
 export default {
   name: '',
   components: {
-
-  }
+    Header,
+    Footer,
+}
 }
 </script>
 
-<style>
+<style lang="less">
 
 </style>
