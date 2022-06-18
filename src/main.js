09efbe31dc37ff0dd91312b16719ccab06b99631
prh <test@qq.com>
2022-06-18 11:19:28 +0800

    kuangjia

--- src/main.js
+++ src/main.js
@@ -1,7 +1,9 @@
 import Vue from 'vue'
 import App from './App.vue'
-import router from "@/router"
 
+import TypeNav from "@/pages/Home/TypeNav"
+Vue.component(TypeNav.name, TypeNav);
+import router from "@/router"
 new Vue({
   render: h => h(App),
   router
--- src/pages/Home/index.vue
+++ src/pages/Home/index.vue
@@ -1,16 +1,35 @@
 <template>
-    <div>
-        wo shi home
-    </div>
+  <div>
+    <TypeNav />
+    <ListContainer />
+    <Recommend />
+    <Rank />
+    <Like />
+    <Floor />
+    <Floor />
+    <Brand />
+  </div>
 </template>
 
 <script>
+import ListContainer from "@/pages/Home/ListContainer";
+import Recommend from "@/pages/Home/Recommend";
+import Rank from "@/pages/Home/Rank";
+import Like from "@/pages/Home/Like";
+import Floor from "@/pages/Home/Floor";
+import Brand from "@/pages/Home/Brand";
 export default {
-    name: '',
-
+  name: "",
+  components: {
+    ListContainer,
+    Recommend,
+    Rank,
+    Like,
+    Floor,
+    Brand,
+  },
 };
 </script>
 
 <style lang="less" scoped>
-
 </style>
