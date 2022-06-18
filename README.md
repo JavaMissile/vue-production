617f0f1ec2ae800f018115965570ae3f7365a5db
prh <test@qq.com>
2022-06-18 18:40:12 +0800

    api

--- README.md
+++ README.md
@@ -3,3 +3,7 @@
 - 初始化文件
 - 测试分支master
 - merge分支main
+
+1.结构规范，命名规范
+2.lang="less"
+3.文件路径
---
+++ src/api/request.js
@@ -0,0 +1,18 @@
+import axios from "axios";
+import nprogress from "nprogress";
+import "nprogress/nprogress.css"
+const requests = axios.create({
+    baseURL: "/api",
+    timeout: 5000,
+});
+requests.interceptors.request.use((config) => {
+    nprogress.start();
+    return config;
+});
+requests.interceptors.response.use((res) => {
+    nprogress.done();
+    return res.data
+}, (error) => {
+    return Promise.reject(new Error('faile'));
+});
+export default requests;
