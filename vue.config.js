db586b34d08b8f6fc5a4f38f420a5af789fa569f
prh <test@qq.com>
2022-06-18 11:25:18 +0800

    api

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,11 @@
-module.exports ={
+module.exports = {
     //关闭eslint
-    lintOnSave:false,
+    lintOnSave: false,
     //代理跨域
-    devServer:{
-        proxy:{
-            '/api':{
-                target:'http://39.98.123.211',
+    devServer: {
+        proxy: {
+            '/api': {
+                target: 'http://gmall-h5-api.atguigu.cn',
                 //pathRewrite:{'^/api':''},
             },
         },
